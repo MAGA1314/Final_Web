ec2f4f6ec7fb260f0f9e0c837b27fe7133ac0217
JM0NT0YA <jh.montoya1@pascualbravo.edu.co>
2024-05-21 22:29:26 -0500

    Productos
    Productos Carrito

--- Proyecto_Final/src/components/Inicio.jsx
+++ Proyecto_Final/src/components/Inicio.jsx
@@ -4,6 +4,16 @@ import appFirebase from '../Credenciales';
 import { getAuth, signOut } from 'firebase/auth';
 import Registro from '../assets/Registro.jpg';
 import Usuario from '../assets/Usuario.jpg';
+import iphone from '../assets/iphone.jpg';
+import celular1 from '../assets/celular1.jpg';
+import celular2 from '../assets/celular2.jpg';
+import celular3 from '../assets/celular3.jpg';
+import celular4 from '../assets/celular4.jpg';
+import celular5 from '../assets/celular5.jpg';
+import celular6 from '../assets/celular6.jpg';
+import celular7 from '../assets/celular7.jpg';
+import celular8 from '../assets/celular8.jpg';
+import celular9 from '../assets/celular9.jpg';
 
 const auth = getAuth(appFirebase);
 
@@ -82,6 +92,66 @@ const Inicio = ({ correoUsuario }) => {
             <img src={Usuario} alt="English Horse" className="img-item" />
             <span className="precio-item">$25.000</span>
             <button className="boton-item" onClick={() => agregarAlCarrito("English Horse", "$25.000", Usuario)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={iphone} alt="iphone" className="img-item" />
+            <span className="precio-item">$75.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular", "$75.000", iphone)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular1} alt="Celular" className="img-item" />
+            <span className="precio-item">$95.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular1", "$95.000", celular1)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular2} alt="Celular" className="img-item" />
+            <span className="precio-item">$95.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular2", "$95.000", celular2)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular3} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular3", "$65.000", celular3)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular4} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular4", "$65.000", celular4)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular5} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular5", "$65.000", celular5)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular6} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular6", "$65.000", celular6)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular7} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular7", "$65.000", celular7)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular8} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular8", "$65.000", celular8)}>Agregar al Carrito</button>
+          </div>
+          <div className="item">
+            <span className="titulo-item">Celular</span>
+            <img src={celular9} alt="Celular" className="img-item" />
+            <span className="precio-item">$65.000</span>
+            <button className="boton-item" onClick={() => agregarAlCarrito("Celular9", "$65.000", celular9)}>Agregar al Carrito</button>
           </div>
         </div>
         {/* Carrito de Compras */}
